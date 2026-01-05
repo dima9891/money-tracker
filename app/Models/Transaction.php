cef30f3720bca29b2bf509d706be9fdb28ea2e82
Дмитрий Резвых <rezvykh22@gmail.com>
2026-01-05 15:23:19 +0300

    update Transaction and TransactionCategory models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Casts\MoneyCast;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'name',
+        'amount',
+        'currency',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'amount' => MoneyCast::class,
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(TransactionCategory::class);
+    }
 }
--- app/Models/TransactionCategory.php
+++ app/Models/TransactionCategory.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionCategory extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function transactions()
+    {
+        return $this->belongsToMany(Transaction::class);
+    }
 }
